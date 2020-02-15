7fcf565ccb9ad0cb804d62305ba1c48f4d60d6d0
vladagurets <aguretsvlad@gmail.com>
2020-02-15 11:21:46 +0200

    init

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { onMount } from 'svelte'
+  import { text } from 'src/store'
+  
+  onMount(() => {
+    setInterval(() => {
+      text.set($text === 'Hello' ? 'World' : 'Hello' )
+    }, 1000)
+  })
+</script>
+
+<style>
+  div {
+    font-size: 50px;
+  }
+</style>
+
+<div>
+  {$text}
+</div>
